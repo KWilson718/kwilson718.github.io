baf254f981b3dd257a7edf22bb44ff44c770fdde
KWilson718 <wilsonk18@spu.edu>
2024-11-13 14:45:35 -0800

    Added basic placeholder message

--- src/views/Misc.vue
+++ src/views/Misc.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="misc">
-        <h3>Misc</h3>
+        <h1>Miscellaneous</h1>
+        <p>This Page will contain various Non-Software-Development related work that is worth sharing.</p>
+        <p>Currently this is empty, however there will be more stuff added soon!</p>
     </div>
 </template>
