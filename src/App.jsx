cb5c1dff7ef215945b84bda876196b7b11180fe1
KWilson718 <wilsonk18@spu.edu>
2025-03-22 22:40:25 -0700

    adding in basic pages for routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import getTheme from "./utils/theme";
 import Navbar from "./components/navbar";
 
 import PageNotFound from "./pages/PageNotFound";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Misc from "./pages/Misc";
 
 export default function App() {
     // Logic for Setting up the correct theme
@@ -25,6 +29,10 @@ export default function App() {
                 <Navbar mode={mode} setMode={setMode} />
                 <Box sx={{ p: 2, bgcolor: "background.default", color: "text.primary" }}>
                     <Routes>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/projects" element={<Projects/>} />
+                        <Route path="/experience" element={<Experience/>} />
+                        <Route path="/misc" element={<Misc/>} />
                         <Route path="*" element={<PageNotFound/>} />
                     </Routes>
                 </Box>
