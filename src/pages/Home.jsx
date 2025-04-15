b4cc668d2d92dd2015d8f58decc04db47b13d981
KWilson718 <wilsonk18@spu.edu>
2025-04-14 19:51:40 -0700

    alignment of first pieces of about page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,41 @@
-import {Avatar} from "@mui/material";
+import {Avatar, Box, Typography} from "@mui/material";
 
 import profilePic from "../assets/photos/profile.png";
 
 export default function Home () {
     return (
         <>
-            <Avatar 
-                alt="Kyle Wilson"
-                src={profilePic}
+            <Box
                 sx={{
-                    width: "30vh",
-                    height: "30vh"
+                    width: "100%",
+                    marginTop: "5vh",
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
                 }}
-            />
+            >
+                <Avatar 
+                    alt="Kyle Wilson"
+                    src={profilePic}
+                    sx={{
+                        width: "40vh",
+                        height: "40vh"
+                    }}
+                />
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column"
+                    }}
+                >
+                    <Typography variant={"h1"}>
+                        Hi, I'm Kyle
+                    </Typography>
+                    <Typography variant={"h5"}>
+                        Welcome to my Portfolio!
+                    </Typography>
+                </Box>
+            </Box>
         </>
     );
 }
