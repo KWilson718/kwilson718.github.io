3a4116ea7b0397aceeb091417acbaaafec4774c8
KWilson718 <wilsonk18@spu.edu>
2025-05-18 21:12:02 -0700

    adding small radius to card

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -2,7 +2,7 @@ import {Card, CardContent, Typography, Chip} from "@mui/material"
 
 export default function ProjectCard({ projectName="", projectDesc="", projectSkills=[] }) {
     return (
-        <Card sx={{ minWidth: "15vw", maxWidth: "50vw" }}>
+        <Card sx={{ minWidth: "15vw", maxWidth: "50vw", borderRadius: "0.5rem" }}>
             <CardContent>
                 <Typography variant="h6" sx={{margin: "0.3rem"}}>{projectName}</Typography>
                 <Typography variant="body2" color="text.secondary" sx={{margin: "0.3rem"}}>
